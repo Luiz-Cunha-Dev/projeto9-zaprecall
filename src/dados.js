b31293ffa6a12875b3b05144358c6074e0dcce87
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2022-10-03 17:01:43 -0300

    Mudança do termo pergunta para flashcard

--- src/dados.js
+++ src/dados.js
@@ -1,12 +1,12 @@
 const dados = [
-    {numero:"Pergunta 1", pergunta:"O que é JSX?", resposta:"Uma extensão de linguagem do JavaScript"},
-    {numero:"Pergunta 2", pergunta:"O React é __", resposta:"Uma biblioteca JavaScript para construção de interfaces"},
-    {numero:"Pergunta 3", pergunta:"Componentes devem iniciar com __ ", resposta:"Letra maiúscula"},
-    {numero:"Pergunta 4", pergunta:"Podemos colocar __ dentro do JSX", resposta:"Expressões"},
-    {numero:"Pergunta 5", pergunta:"O ReactDOM nos ajuda __ ", resposta:"Interagindo com a DOM para colocar componentes React na mesma"},
-    {numero:"Pergunta 6", pergunta:"Usamos o npm para __ ", resposta:"Gerenciar os pacotes necessários e suas dependências"},
-    {numero:"Pergunta 7", pergunta:"Usamos props para __ ", resposta:"Passar diferentes informações para componentes"},
-    {numero:"Pergunta 8", pergunta:"Usamos estado (state) para __ ", resposta:"Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}
+    {numero:"FlashCard 1", pergunta:"O que é JSX?", resposta:"Uma extensão de linguagem do JavaScript"},
+    {numero:"FlashCard 2", pergunta:"O React é __", resposta:"Uma biblioteca JavaScript para construção de interfaces"},
+    {numero:"FlashCard 3", pergunta:"Componentes devem iniciar com __ ", resposta:"Letra maiúscula"},
+    {numero:"FlashCard 4", pergunta:"Podemos colocar __ dentro do JSX", resposta:"Expressões"},
+    {numero:"FlashCard 5", pergunta:"O ReactDOM nos ajuda __ ", resposta:"Interagindo com a DOM para colocar componentes React na mesma"},
+    {numero:"FlashCard 6", pergunta:"Usamos o npm para __ ", resposta:"Gerenciar os pacotes necessários e suas dependências"},
+    {numero:"FlashCard 7", pergunta:"Usamos props para __ ", resposta:"Passar diferentes informações para componentes"},
+    {numero:"FlashCard 8", pergunta:"Usamos estado (state) para __ ", resposta:"Dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}
 ]
 
 export default dados;
