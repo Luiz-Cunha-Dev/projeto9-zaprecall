ead1849a0644998db194f6ca72b1210fd57a6697
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2022-10-03 16:45:32 -0300

    pequena correção layout

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,6 @@ import styled from "styled-components"
 export default function Footer(props){
     return(
         <FooterConcluidos>
-
             <span>{props.numeroConcluidos}/8 concluidos</span>
             <Resutado>
             {props.children}
@@ -16,7 +15,6 @@ const Resutado = styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
-height: 30px;
 img{
   height: 23px;
   width: 23px;
